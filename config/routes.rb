53fb58b65a060eb6b910288f4c156c42f2205d85
saurabh upadhyay <saurabh.upadhyay@coupa.com>
2019-12-12 23:25:22 -0800

    customized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-
-  get 'pages/about'
-  
-  get 'pages/contact'
-
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #get 'pages/contact'
+  get 'about-me', to: 'pages#about'    #### call localhost:3000/about  to call about page
+  #get 'pages/contact'        #### call localhost:3000/contacts
+  get 'contact', to: 'pages#contact'
+
+  #get 'pages/home'
+  root to: 'pages#home'
 end
